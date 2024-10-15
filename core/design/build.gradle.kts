47c50770407961395f1e75f6c1193e8bef9a3813
romeljordan <romeljordan25@gmail.com>
2024-10-15 22:30:22 +0800

    Add app colors

--- core/design/build.gradle.kts
+++ core/design/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ core/design/src/main/java/com/demo/app/core/design/theme/AppColor.kt
@@ -0,0 +1,11 @@
+package com.demo.app.core.design.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val primaryBlue = Color(0xFF023047)
+    val lightBlue = Color(0xFF8ECAE6)
+    val blue = Color(0xFF023047)
+    val yellow = Color(0xFFFFB703)
+    val orange = Color(0xFFFB8500)
+}
