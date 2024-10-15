fc29b4cdffaf2d6045c680c1c55d5b6c7301b028
romeljordan <romeljordan25@gmail.com>
2024-10-16 07:24:55 +0800

    Rename fonts

--- core/design/src/main/java/com/demo/app/core/design/font/AppFont.kt
+++ core/design/src/main/java/com/demo/app/core/design/font/AppFont.kt
@@ -7,13 +7,13 @@ import com.demo.app.core.design.R
 
 val poppinsFontFamily = FontFamily(
     listOf(
-        Font(R.font.Poppins_Black, weight = FontWeight.Black),
-        Font(R.font.Poppins_ExtraBold, weight = FontWeight.ExtraBold),
-        Font(R.font.Poppins_SemiBold, weight = FontWeight.SemiBold),
-        Font(R.font.Poppins_Bold, weight = FontWeight.Bold),
-        Font(R.font.Poppins_Medium, weight = FontWeight.Medium),
-        Font(R.font.Poppins_Regular, weight = FontWeight.Normal),
-        Font(R.font.Poppins_Light, weight = FontWeight.Light),
-        Font(R.font.Poppins_Thin, weight = FontWeight.Thin),
+        Font(R.font.poppins_black, weight = FontWeight.Black),
+        Font(R.font.poppins_extrabold, weight = FontWeight.ExtraBold),
+        Font(R.font.poppins_semibold, weight = FontWeight.SemiBold),
+        Font(R.font.poppins_bold, weight = FontWeight.Bold),
+        Font(R.font.poppins_medium, weight = FontWeight.Medium),
+        Font(R.font.poppins_regular, weight = FontWeight.Normal),
+        Font(R.font.poppins_light, weight = FontWeight.Light),
+        Font(R.font.poppins_thin, weight = FontWeight.Thin),
     )
 )
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -23,15 +23,18 @@ android {
             )
         }
     }
-    buildFeatures {
-        compose = true
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
