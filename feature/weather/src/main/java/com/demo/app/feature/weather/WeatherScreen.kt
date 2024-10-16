c6440d177c86c1a69b86fab1c53d917a8701c3f8
romeljordan <romeljordan25@gmail.com>
2024-10-16 23:36:27 +0800

    Update WeatherScreen.kt

--- feature/weather/src/main/java/com/demo/app/feature/weather/WeatherScreen.kt
+++ feature/weather/src/main/java/com/demo/app/feature/weather/WeatherScreen.kt
@@ -1,8 +1,38 @@
 package com.demo.app.feature.weather
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.demo.app.core.design.theme.appTypography
 
 @Composable
 internal fun WeatherScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Hello, username!",
+                style = appTypography.titleLarge.copy(
+                    color = Color.White
+                )
+            )
 
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewWeatherScreen() {
+    WeatherScreen()
 }
